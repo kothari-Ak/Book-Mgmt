634f1b54128b99d2c2a2f43b14f39ec10bd50d41
animeshj2132 <ajangir52@gmail.com>
2022-07-06 19:17:18 +0530

    middleware

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../Controllers/userController')
+const Authentication = require("../middlewares/authentication")
 // const bookController = require('../Controllers/bookController')
 
 router.post("/register", userController.createUser)

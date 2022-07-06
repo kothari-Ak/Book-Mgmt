07f836103c4c34fed4224d6f2a3b9047851fdcbd
animeshj2132 <ajangir52@gmail.com>
2022-07-06 11:48:12 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {default: mongoose} = require('mongoose');
+const multer = require ('multer')
+
+const route = require('./routes/route')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(multer().any())
+
+mongoose.connect("mongodb+srv://animesh:KNlVl9CDDcfmXfF0@cluster0.nrkqb.mongodb.net/group8Database",{useNewUrlParser: true})
+.then(()=>console.log("MongoDb is connected"))
+.catch(error=>console.log(error))
+
+app.use('/', route)
+
+app.listen(process.env.PORT || 3000, function (){
+     console.log('Express app running on port' + (process.env.PORT || 3000))
+});
